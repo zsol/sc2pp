23f5768197d7f04b3ddcd8b2083248ec9d9abcd5
Zsolt Dollenstein <zsol.zsol@gmail.com>
2011-11-15 20:17:42 +0100

    enhance detailsinfo tool to be able to read from multiple inputs and mpq files

--- tools/detailsinfo.cpp
+++ tools/detailsinfo.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <memory>
 #include <sc2pp/types.h>
 #include <sc2pp/parsers.h>
 
@@ -9,14 +11,25 @@
 using namespace std;
 namespace po = boost::program_options;
 
+template <typename Iterator>
+void parse(Iterator begin, Iterator end)
+{
+    sc2pp::object_type details;
+    parse(begin, end, sc2pp::parsers::object, details);
+    cout << details << endl;
+}
+
 int main(int argc, char** argv)
 {
+    const size_t BUFSIZE = 1 << 20; // 1MB
+
     po::options_description desc("Possible options");
     po::positional_options_description posdesc;
     po::variables_map vm;
 
     desc.add_options()
         ("help", "display this help message")
+        ("input", po::value<vector<string>>(), "input file(s)")
         ("mpq", "treat input file as an MPQ archive (SC2Replay file), not as the replay.details file")
         ;
     posdesc.add("input", -1);
@@ -29,21 +42,66 @@ int main(int argc, char** argv)
 
     if (vm.count("input") == 0)
     {
-        const size_t BUFSIZE = 1 << 20; // 1MB
+        if (vm.count("mpq"))
+        {
+            std::cerr << "Reading MPQ archives from stdin is not supported. Specify filename as an argument!" << std::endl;
+            return 1;
+        }
+
         typedef array<unsigned char, BUFSIZE> buffer_type;
         buffer_type buf;
         auto tobegin = begin(buf), toend = end(buf);
-        cin >> noskipws;
+        cin >> noskipws; // don't swallow \n, \t, etc
         istream_iterator<char> frombegin(cin), fromend;
         size_t actual_size = 0;
         while (frombegin != fromend && tobegin != toend)
             *tobegin++ = *frombegin++, ++actual_size;
 
-        sc2pp::object_type details;
-        parse(buf.cbegin(), buf.cbegin()+actual_size, sc2pp::parsers::object, details);
-        std::cout << details << std::endl;
+        parse(buf.cbegin(), buf.cbegin()+actual_size);
+        return 0;
     }
 
+    const auto& inputs = vm["input"].as<vector<string>>();
+    for (const auto& input : inputs)
+    {
+        unique_ptr<unsigned char[]> buf;
+        size_t actual_size = 0;
+        if (vm.count("mpq"))
+        {
+            mpq_archive_s* archive;
+            int ret = libmpq__archive_open(&archive, input.c_str(), -1);
+            if (ret != 0)
+            {
+                cerr << "Unable to open MPQ archive: " << input << endl;
+                return 2;
+            }
+            unsigned int fileno = 0;
+            long size = 0, asize = 0;
+            libmpq__file_number(archive, "replay.details", &fileno);
+            libmpq__file_unpacked_size(archive, fileno, &size);
+            buf.reset(new unsigned char[size]);
+            libmpq__file_read(archive, fileno, buf.get(), size, &asize);
+            actual_size = asize;
+        }
+        else
+        {
+            ifstream file(input, ios_base::in | ios_base::binary);
+            if (not file.good())
+            {
+                cerr << "Unable to open file: " << input << endl;
+                return 2;
+            }
+            
+            buf.reset(new unsigned char[BUFSIZE]);
+            file >> noskipws;
+            istream_iterator<char> frombegin(file), fromend;
+            unsigned char* tobegin = buf.get(), *toend = buf.get()+BUFSIZE;
+            while (frombegin != fromend && tobegin != toend)
+                *tobegin++ = *frombegin++, ++actual_size;            
+        }
+        const unsigned char* begin = buf.get();
+        parse(begin, begin+actual_size);
+    }
 
     return 0;
 }
